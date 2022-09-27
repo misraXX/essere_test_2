da17151bcc15d0e32d5e15af2c797304ffafa290
misraXX <107029489+misraXX@users.noreply.github.com>
2022-09-28 03:24:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ USER_ID = info['USER_ID']
 
 
 from selenium import webdriver
-import chromedriver_binary
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
