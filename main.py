aac088a1eb1b4f7d1152bed7c9edf018b6779a2a
misraXX <107029489+misraXX@users.noreply.github.com>
2022-09-28 03:19:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     driver.find_element(By.CSS_SELECTOR,'#passwordloginForm').send_keys(password)
     driver.find_element(By.CSS_SELECTOR,'#loginSubmit_').click()
     #ほしい商品の入荷状況を確認
-    url = "https://jp.louisvuitton.com/jpn-jp/products/neverfull-mm-monogram-empreinte-nvprod2790031v/M46135"
+    url = "https://jp.louisvuitton.com/jpn-jp/products/pochette-voyage-epi-nvprod3660137v/M81326"
     driver.get(url)
     #指定した要素がクリックできる状態になるまで待機
     time.sleep(5)
